04f334a686d3b39f80a0c7e86627a5b998447957
MajidEi <abdelmajededdahbi@gmail.com>
2021-06-24 14:22:35 +0100

    fixed date ona

--- src/app/rdv/components/rdv/rdv.component.ts
+++ src/app/rdv/components/rdv/rdv.component.ts
@@ -45,7 +45,7 @@ export class RdvComponent implements OnInit {
 		this.d = new Date().toISOString().split('T')[0];
 		console.log(this.d);
 		console.log(datederdv);
-		if (datederdv.split('-')[0] > this.d.split('-')[0]) {
+		if (parseInt(datederdv.split('-')[0],10) > parseInt(this.d.split('-')[0],10)) {
 			return false;
 		} else {
 			return true;
